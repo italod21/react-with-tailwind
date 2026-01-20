9f608e582fbbdff36acecd48c63a32211a4f4a29
Italo Duarte <italod21@gmail.com>
2026-01-20 14:44:20 -0300

    feat: create the project base structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,15 @@
 
 export default function App() {
-  return "Hello React!";
+  return (
+  <header>
+    <a href="/">bikraft</a>
+    <nav>
+      <ul>
+        <li><a href="/products">products</a></li>
+        <li><a href="/about-us">about us</a></li>
+        <li><a href="/contact">contact</a></li>
+      </ul>
+    </nav>
+  </header>
+);
 }
