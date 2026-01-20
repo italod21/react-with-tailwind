abaae5c4f48fd39debef1c512d936f4bfb1e25dc
Italo Duarte <italod21@gmail.com>
2026-01-20 16:42:52 -0300

    style: add header style

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 
 export default function App() {
   return (
-  <header>
-    <a href="/">bikraft</a>
+  <header className="bg-black flex items-center justify-between text-amber-50 px-40 py-7">
+    <a href="/" className="text-3xl font-semibold tracking-wider">bikcraft</a>
     <nav>
-      <ul>
-        <li><a href="/products">products</a></li>
-        <li><a href="/about-us">about us</a></li>
-        <li><a href="/contact">contact</a></li>
+      <ul className="flex justify-between gap-8">
+        <li className="hover:bg-yellow-600 px-4 py-2 rounded-[10px] capitalize"><a href="/products">products</a></li>
+        <li className="hover:bg-yellow-600 px-4 py-2 rounded-[10px] capitalize"><a href="/about-us">about us</a></li>
+        <li className="hover:bg-yellow-600 px-4 py-2 rounded-[10px] capitalize"><a href="/contact">contact</a></li>
       </ul>
     </nav>
   </header>
